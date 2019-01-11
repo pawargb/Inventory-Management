c93d083491bfc72bad75651bc00be04636479914
Ganesh <pawargb.2071990@gmail.com>
2019-01-11 07:06:25 +0530

    Initial Commit

---
+++ Inventory Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Inventory Management
+//
+//  Created by Ganesh Balaji Pawar on 11/01/19.
+//  Copyright © 2019 Ganesh Balaji Pawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
